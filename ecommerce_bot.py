e8fb052fb2f8523a3e22c20e278a9ca245cb63db
shemeka <st657@rutgers.edu>
2025-05-19 19:03:06 -0400

    Use env variable for Google credentials

--- ecommerce_bot.py
+++ ecommerce_bot.py
@@ -4,7 +4,12 @@ import re
 
 # Google Sheets setup
 scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-creds = ServiceAccountCredentials.from_json_keyfile_name('credentials.json', scope)
+import json
+import os
+
+creds_json = os.getenv('GOOGLE_CREDENTIALS')
+creds_dict = json.loads(creds_json)
+creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
 client = gspread.authorize(creds)
 sheet = client.open("DiscountGalaFAQ").sheet1
 
